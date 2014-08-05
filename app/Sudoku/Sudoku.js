d0021b47cd2c34b7766fa7a033abe34100a8ca6b
Tom Kuhn <tom.c.kuhn@gmail.com>
2014-08-05 15:25:32 +0100

    UI tweaks
    Added completion message.
    Fixed Firefox keyboard entry issues.

--- app/Sudoku/Sudoku.js
+++ app/Sudoku/Sudoku.js
@@ -1,50 +1,20 @@
 (function() {
 
   function sudokuController($scope, SudokuPuzzleService, RandomNumberService) {
-    // Stores the values entered by the user
-    $scope.board = [
-      [, , , , , , , , ],
-      [, , , , , , , , ],
-      [, , , , , , , , ],
-      [, , , , , , , , ],
-      [, , , , , , , , ],
-      [, , , , , , , , ],
-      [, , , , , , , , ],
-      [, , , , , , , , ],
-      [, , , , , , , , ]
-    ];
-
-    // Stores up any markup entered by the user
-    $scope.markings = [
-      [, , , , , , , , ],
-      [, , , , , , , , ],
-      [, , , , , , , , ],
-      [, , , , , , , , ],
-      [, , , , , , , , ],
-      [, , , , , , , , ],
-      [, , , , , , , , ],
-      [, , , , , , , , ],
-      [, , , , , , , , ]
-    ];
-
-    $scope.puzzle = [
-      [4, , , , , , 8, , 5],
-      [, 3, , , , , , , ],
-      [, , , 7, , , , , ],
-      [, 2, , , , , , 6, ],
-      [, , , , 8, , 4, , ],
-      [, , , , 1, , , , ], 
-      [, , , 6, , 3, , 7, ],
-      [5, , , 2, , , , , ],
-      [1, , 4, , , , , , ]
-    ];
+    
+    $scope.board =  null; // Stores the values entered by the user    
+    $scope.markings = null; // Stores up any markup entered by the user
+    $scope.puzzle = null; // Stores the initial puzzle state
+    $scope.solution = null; // Stores the complete solution to the puzzle
 
     $scope.hoverColumn = -1;
-
     $scope.selectedCell = null;
-
     $scope.MarkupCells = false;
+    $scope.completed = false;
 
+    /*
+     * Initialises the board variables to be empty
+     */
     $scope.init = function(){
       // Stores the values entered by the user
       $scope.board = [
@@ -96,21 +66,19 @@
         [, , , , , , , , ]
       ];
 
+      $scope.completed = false;
     };
 
-    $scope.NewGame = function(){
+    /*
+     * Creates a new game based on the selected difficulty (or a
+     *  random difficulty if none was selected).
+     */
+    $scope.RandomNewGame = function(difficulty){
       $scope.init();
 
-      $scope.solution = SudokuPuzzleService.NewGame( -1 );
-
-      $scope.puzzle = SudokuPuzzleService.GeneratePuzzle( $scope.solution, -1, $scope.puzzleDifficulty );
-    };
-
-    $scope.RandomNewGame = function(difficulty){
-
+      // Generate a random difficulty level if none was selected
       var rand = RandomNumberService.GetRandomInt(0,3);
-      var randDifficulty =  SudokuPuzzleService.DifficultyLevel.EASY;
-      
+      var randDifficulty =  SudokuPuzzleService.DifficultyLevel.EASY;      
       switch(rand){
       case 0:
         randDifficulty = SudokuPuzzleService.DifficultyLevel.HARD;
@@ -123,11 +91,16 @@
         break;
       }
 
-      $scope.puzzleDifficulty = difficulty || randDifficulty;
+      $scope.puzzleDifficulty = difficulty || randDifficulty;      
 
-      $scope.NewGame();
+      $scope.solution = SudokuPuzzleService.NewGame( -1 );
+
+      $scope.puzzle = SudokuPuzzleService.GeneratePuzzle( $scope.solution, -1, $scope.puzzleDifficulty );
     };
 
+    /*
+     * Toggles the ability to edit the puzzle's markup
+     */
     $scope.ToggleCellMarkup = function () {
       $scope.MarkupCells = !$scope.MarkupCells;
     };
@@ -169,6 +142,22 @@
     };
 
     /*
+     * Checks the game for a completed state and sets the 'Completed'
+    * flag if true.
+     */
+    $scope.CheckBoardForCompletion = function(){
+      for(var i = 0; i < 9; i++){
+        for(var j = 0; j < 9; j++){
+          if(!$scope.GetBoardValue(i,j)){
+            return;
+          }
+        };
+      };
+      
+      $scope.completed = true;
+    };
+ 
+    /*
      * Sets the Selected Cell value on the board
      */
     $scope.SetSelectedCellValue = function (value) {
@@ -200,15 +189,25 @@
       };
 
       $scope.board[x][y] = value;
+
+      // If the board has been filled ...
+      $scope.CheckBoardForCompletion();
     };
 
     /*
      * Handles keyboard input to enter values on the board
      */
     $scope.handleKeyboardEntry = function ($event) {
+      var value = $event.keyCode;
+      
+      if(value == 0){
+        // Handle firefox input
+        value = $event.charCode;
+      };
+
       // If a number has been pressed
-      if ($event.keyCode > 48 && $event.keyCode < 58) {
-        $scope.SetSelectedCellValue($event.keyCode - 48);
+      if (value > 48 && value < 58) {
+        $scope.SetSelectedCellValue(value - 48);
       };
     };
 
@@ -216,19 +215,25 @@
      * Handles the request to toggle on/off the board markup
      */
     $scope.handleKeyboardKeyDown = function ($event) {
+      var value = $event.keyCode;
+      
+      if(value == 0){
+        // Handle firefox input
+        value = $event.charCode;
+      };
+
       // If Control has been pressed to toggle markup
-      if ($event.keyCode == 17) {
+      if (value == 17) {
         $scope.ToggleCellMarkup();
         return;
       };
 
-
       // If the cell should be deleted
-      if ($event.keyCode == 46 || $event.keyCode == 8) {
+      if (value == 46 || value == 8) {
         $scope.SetSelectedCellValue(null);
 
         // Stop the page from navigating backwards
-        if ($event.keyCode == 8) {
+        if (value == 8) {
           $event.preventDefault();
         };
       };
@@ -319,7 +324,6 @@
       return false;
     };
 
-
     // Call this to initialize a new game
     RandomNumberService.SetSeed(1);
     $scope.RandomNewGame();
@@ -360,26 +364,44 @@
     }  
   };
 
+  /*
+   * A service that is responsible for generating Sudoku puzzles and solutions
+   */
   function sudokuPuzzleService(RandomNumberService){
+    // Enum defining the difficulty levels available
     this.DifficultyLevel = {
       EASY: "Easy",
       MEDIUM: "Medium",
       HARD: "Hard"
     };
 
+    /*
+     * Gets a valid swap target index based on the source index. I.e.
+     * If swapping column 1, then the only other valid columns to swap
+     * with are 0 and 2. If any other columns are selected, then this
+     * would produce an invalid Sudoku board.
+     */
     function getValidPuzzleTargetSwapIndex (puzzleSourceIndex){
-      var puzzleBlockIndex = puzzleSourceIndex % 3;
+      // Gets the index of the source within the 3x9 block
+      // e.g. if col 8 is the source, then the index in the 3rd block
+      // would be 2
+      var puzzleBlockIndex = puzzleSourceIndex % 3; 
       var result = puzzleBlockIndex;
 
+      // Generate a target index that is different from the source index
       while(result == puzzleBlockIndex)
       {
         result = RandomNumberService.GetRandomInt(0,2);
       }
 
+      // Convert the target index back into the puzzle index
       result = (puzzleSourceIndex - puzzleBlockIndex) + result;
       return result;
     };
 
+    /*
+     * Swaps two 3x9 columnm or row blocks within the Sudoku puzzle
+     */
     function swapBlock(puzzle, isColumn){
       
       var sourceBlock = RandomNumberService.GetRandomInt(0,2);
@@ -397,27 +419,36 @@
       }
     };
 
+    /*
+     * Swaps two rows within the Sudoku puzzle
+     */
     function swapRow(puzzle){
       var sourceIndex = RandomNumberService.GetRandomInt(0,8);
       var targetIndex = getValidPuzzleTargetSwapIndex(sourceIndex);
 
-      var temp = puzzle[sourceIndex];
+      var temp = puzzle[sourceIndex]; // Store the source row temporarily
       puzzle[sourceIndex] = puzzle[targetIndex];
       puzzle[targetIndex] = temp;
       
     };
 
+    /*
+     * Swaps two columns within the Sudoku puzzle
+     */
     function swapColumn(puzzle){
       var sourceIndex = RandomNumberService.GetRandomInt(0,8);
       var targetIndex = getValidPuzzleTargetSwapIndex(sourceIndex);
 
       for(var j = 0; j < 9; j++){
-        var buffer = puzzle[j][sourceIndex];
+        var buffer = puzzle[j][sourceIndex]; // Store the source col temporarily
         puzzle[j][sourceIndex] = puzzle[j][targetIndex];
         puzzle[j][targetIndex] = buffer;
       };
     };
 
+    /*
+     * Greates a new Sudoku solution for the page
+     */
     this.NewGame = function(seed){
       if(seed < 1){
         seed = Math.floor(RandomNumberService.Random(true) * (65535 - 1)) + 1;
@@ -425,7 +456,8 @@
 
       RandomNumberService.SetSeed(seed);
 
-      initialPuzzle = [
+      // An initial valid 'default' sudoku puzzle
+      result = [
         [1, 2, 3, 4, 5, 6, 7, 8, 9],
         [4, 5, 6, 7, 8, 9, 1, 2, 3],
         [7, 8, 9, 1, 2, 3, 4, 5, 6],
@@ -437,30 +469,38 @@
         [9, 7, 8, 5, 3, 1, 6, 4, 2]
       ];
       
-      var maxGenerations  = RandomNumberService.GetRandomInt(400,800);
+      var maxGenerations = RandomNumberService.GetRandomInt(400,800);
 
+      // Transform the puzzle for 'n' number of generations
       for(var i = 0; i < maxGenerations; i++){
         var transformOption = RandomNumberService.GetRandomInt(0,4);
 
+        // Apply the transform to the result. Each transform results
+        // in a valid sudoku board
         switch(transformOption){
         case 0 :
-          swapRow(initialPuzzle);          
+          swapRow(result);          
           break;
         case 1 :
-          swapColumn(initialPuzzle);          
+          swapColumn(result);          
           break;
         case 2:
-          swapBlock(initialPuzzle, true);
+          swapBlock(result, true);
           break;
         case 3:
-          swapBlock(initialPuzzle, false);
+          swapBlock(result, false);
           break;
         };
       };
 
-      return initialPuzzle;
+      return result;
     };
 
+    /*
+     * Takes the initial Sudoku puzzle and removes a random quantity
+     * of numbers in order to produce a valid puzzle. This may reesult
+     * in a puzzle that is in fact impossible to solve.
+     */
     this.GeneratePuzzle = function( solution, seed, puzzleDifficulty ){
       if(seed < 1){
         seed = Math.floor(RandomNumberService.Random(true) * (65535 - 1)) + 1;
@@ -468,12 +508,11 @@
 
       RandomNumberService.SetSeed(seed);
 
+      // Set the solution retention rate based on the difficulty selected
       var percentageOfNumbersToKeep = 45;
-
       if(puzzleDifficulty == this.DifficultyLevel.MEDIUM){
         percentageOfNumbersToKeep = 35;
       }
-
       if(puzzleDifficulty == this.DifficultyLevel.HARD){
         percentageOfNumbersToKeep = 25;
       }
@@ -490,6 +529,8 @@
         [, , , , , , , , ]
       ];
 
+      // Selectivly copy values from the solution to the puzzle based
+      // on the percentage of numbers to keep.
       for(var i = 0; i < 9 ; i++){
         for(var j = 0; j < 9; j++){
           var rand = RandomNumberService.GetRandomInt(1,100);
